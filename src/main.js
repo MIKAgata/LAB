3b7caab00e29ff060ea8448d127347f03d2155b0
MIKE <mikael.agata.sitepu@gmail.com>
2026-03-21 14:54:55 +0700

    modified main.js

--- src/main.js
+++ src/main.js
@@ -2,5 +2,14 @@ import {Home} from './pages/home.js'
 import {About} from './pages/about.js'
 
 const routes = {
-    "/" :
+    "/" : Home,
+    "about" : About
 }
+
+function router() {
+    const path = window.location.patchname
+    const page = routes[path] || (() => "<h1>404</h1>")
+
+    document.getElementById("app").innerHTML = page()
+}
+
